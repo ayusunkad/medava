def41287c3571bad5a9d9946706d9b5c43733fa9
ayusunkad <112365869+ayusunkad@users.noreply.github.com>
2023-11-15 13:41:50 -0500

    Update Transporter.java

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,48 +1,55 @@
 package edu.uc.cs3003.medava;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+
+// Transporter class represents a vehicle for shipping and transporting medical goods.
 public class Transporter {
 
+    // Constructor to initialize a Transporter with a name and temperature range.
     public Transporter(String transporterName, double lowTemp, double highTemp) {
         mTransporterName = transporterName;
         mLowTemperature = lowTemp;
         mHighTemperature = highTemp;
     }
 
+    // Instance initialization block to create a new ArrayList for storing goods.
     {
         goods = new ArrayList<Shippable>();
     }
 
+    // Private fields to store transporter name, goods, and temperature range.
     private String mTransporterName;
-
     private List<Shippable> goods;
-
     private double mLowTemperature, mHighTemperature;
 
+    // Getter method to retrieve the name of the transporter.
     public String getTransporterName() {
         return mTransporterName;
     }
 
+    // Method representing the shipping process. (Placeholder comment)
     public void ship() {
-        // Do some shipping!
-      }
+        // Do some shipping! (Placeholder comment)
+    }
 
-      public Shippable unload() {
+    // Method to unload the first item from the goods list.
+    public Shippable unload() {
         return goods.remove(0);
     }
 
+    // Method to load a Shippable item onto the transporter, checking temperature range.
     public boolean load(Shippable itemToLoad) {
         if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
             return goods.add(itemToLoad);
         }
+        // Return false if the item's temperature is not within the acceptable range.
         return false;
-    } 
+    }
+
+    // Method to check if the transporter is empty.
     public boolean isEmpty() {
         return goods.isEmpty();
     }
-
-
-
 }
