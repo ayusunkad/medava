fc8e003bb2ef5ecd8d2a996c489ac053736c93d3
ayusunkad <112365869+ayusunkad@users.noreply.github.com>
2023-11-15 13:44:03 -0500

    Update Pharmacy.java

--- src/main/java/edu/uc/cs3003/medava/Pharmacy.java
+++ src/main/java/edu/uc/cs3003/medava/Pharmacy.java
@@ -1,16 +1,22 @@
 package edu.uc.cs3003.medava;
 
 public class Pharmacy {
+
+    // Constructor to initialize a Pharmacy with a given name.
     public Pharmacy(String pharmacyName) {
         mPharmacyName = pharmacyName;
     }
+
+    // Method to send various medicines and a medical device using a given transporter.
     public boolean send(Transporter t) {
+        // Create an instance of Ibuprofen and attempt to load it onto the transporter.
         Medicine advil = new Ibuprofen();
         if (t.load(advil)) {
             System.out.println(String.format("Sending %s on the %s transporter.", advil.getMedicineName(),
                     t.getTransporterName()));
         } else {
-            System.out.println(String.format("Cannot load %s on to the %s transporter.", advil.getMedicineName(),
+            // Print a message if Ibuprofen cannot be loaded onto the transporter and return false.
+            System.out.println(String.format("Cannot load %s onto the %s transporter.", advil.getMedicineName(),
                     t.getTransporterName()));
             return false;
         }
@@ -20,7 +26,7 @@ public class Pharmacy {
             System.out.println(String.format("Sending %s on the %s transporter.", activase.getMedicineName(),
                     t.getTransporterName()));
         } else {
-            System.out.println(String.format("Cannot load %s on to the %s transporter.", activase.getMedicineName(),
+            System.out.println(String.format("Cannot load %s onto the %s transporter.", activase.getMedicineName(),
                     t.getTransporterName()));
             return false;
         }
@@ -30,7 +36,7 @@ public class Pharmacy {
             System.out.println(String.format("Sending %s on the %s transporter.", oxycontin.getMedicineName(),
                     t.getTransporterName()));
         } else {
-            System.out.println(String.format("Cannot load %s on to the %s transporter.", oxycontin.getMedicineName(),
+            System.out.println(String.format("Cannot load %s onto the %s transporter.", oxycontin.getMedicineName(),
                     t.getTransporterName()));
             return false;
         }
@@ -40,17 +46,20 @@ public class Pharmacy {
             System.out.println(String.format("Sending %s on the %s transporter.", heart.getMedicineName(),
                     t.getTransporterName()));
         } else {
-            System.out.println(String.format("Cannot load %s on to the %s transporter.", heart.getMedicineName(),
+            System.out.println(String.format("Cannot load %s onto the %s transporter.", heart.getMedicineName(),
                     t.getTransporterName()));
             return false;
         }
 
+        // Return true if all items are successfully loaded onto the transporter.
         return true;
     }
 
+    // Getter method to retrieve the name of the pharmacy.
     public String pharmacyName() {
         return mPharmacyName;
     }
 
+    // Private field to store the name of the pharmacy.
     private String mPharmacyName;
 }
